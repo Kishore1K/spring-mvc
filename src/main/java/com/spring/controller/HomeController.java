b0936f5baadd2c78c4ea06ae4d7371ad4985170c
Kishore1K <reddykishore527@gmail.com>
2023-10-12 13:19:45 +0530

    @PathVaribale

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -36,6 +37,11 @@ public class HomeController {
 		return "about";
 	}
 	
+	@RequestMapping("/test/{name}")
+	public String pathHandler(@PathVariable("name") String name) {
+		System.out.println("Hello "+ name);
+		return "home";
+	}
 	
 	@RequestMapping("/help")
 	public ModelAndView help() {
